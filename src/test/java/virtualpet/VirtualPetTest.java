3c9ac997a74d9f8b22de0c1b9d859121a5c5b6ec
SusanWendt <wendt.37@osu.edu>
2018-01-26 18:44:56 -0500

    thirst spelling error fixed

--- src/test/java/virtualpet/VirtualPetTest.java
+++ src/test/java/virtualpet/VirtualPetTest.java
@@ -6,14 +6,17 @@ import org.junit.Test;
 public class VirtualPetTest {
 	@Test
 	public void shouldDecreaseHungerBy4() {
-		// arrange
-		VirtualPet underTest = new VirtualPet(5, 5, 5, 5);
-
-		// act
-		underTest.feed(4);
+		VirtualPet underTest = new VirtualPet(5, 5, 5, 5); // arrange
+		underTest.feed(4); // act
 		int hunger = underTest.getHunger();
+		Assert.assertEquals(1, hunger); // assert
+	}
 
-		// assert
-		Assert.assertEquals(1 , hunger);
+	@Test
+	public void shouldDecreaseThirstBy4() {
+		VirtualPet underTest = new VirtualPet(5, 5, 5, 5);
+		underTest.water(4);
+		int thirst = underTest.getThirst();
+		Assert.assertEquals(1, thirst);
 	}
 }
