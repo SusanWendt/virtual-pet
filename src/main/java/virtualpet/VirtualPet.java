e5ee6bd6af8c41c79502accbbf164503b46a452b
SusanWendt <wendt.37@osu.edu>
2018-01-26 11:21:09 -0500

    integers declared

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -1,5 +1,12 @@
 package virtualpet;
 
 public class VirtualPet {
+	// 1) Create a tick() method that represents the passage of time.
+	// 2) Create at least three instance variables (aka attributes aka properties aka fields).
+	// 3) Create at least three methods (messages you send to your pet).
+	
+	int hunger = feed it;
+	int thirst = water it;
+	int bathroom = let it out to the bathroom;
 
 }
--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -3,8 +3,12 @@ package virtualpet;
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Create a main method that…
+		// 1) implements a game loop.
+		// 2) asks for user input.
+		// 3) writes output to the console.
 
+		System.out.println("Welcome to SoozaPalooza Pet Palace! Please give your pet a name:");
 	}
 
 }
