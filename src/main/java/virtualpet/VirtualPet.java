d64d61ed2d04250be8cc62043cb7d0e28b028236
SusanWendt <wendt.37@osu.edu>
2018-01-26 17:27:43 -0500

    formatting fix

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -2,7 +2,8 @@ package virtualpet;
 
 public class VirtualPet {
 	// 1) Create a tick() method that represents the passage of time.
-	// 2) Create at least three instance variables (aka attributes aka properties aka fields).
+	// 2) Create at least three instance variables (aka attributes aka properties
+	// aka fields).
 	// 3) Create at least three methods (messages you send to your pet).
 
 	// instance data
@@ -36,7 +37,7 @@ public class VirtualPet {
 	public int getEnergy() {
 		return energy;
 	}
-	
+
 	// Modifier Method
 	public void feed(int amountToFeed) {
 		// hunger = hunger - amountToFeed; AKA below line
@@ -62,18 +63,20 @@ public class VirtualPet {
 			bathroom -= 0;
 		}
 	}
-		public void play(int amountToEnergy) {
-			if (amountToEnergy > 0) {
-				energy -= amountToEnergy;
-			} else {
-				energy -= 0;
-			}
-	}
-		public void tick() {
-			hunger += 1;
-			thirst += 1;
-			bathroom += 1;
-			energy +=1;
-			
+
+	public void play(int amountToEnergy) {
+		if (amountToEnergy > 0) {
+			energy -= amountToEnergy;
+		} else {
+			energy -= 0;
 		}
+	}
+
+	public void tick() {
+		hunger += 1;
+		thirst += 1;
+		bathroom += 1;
+		energy += 1;
+
+	}
 }
--- src/test/java/virtualpet/VirtualPetTest.java
+++ src/test/java/virtualpet/VirtualPetTest.java
@@ -1,18 +1,19 @@
 package virtualpet;
 
-//import org.junit.Assert;
-//import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class VirtualPetTest {
-//	@Test
-//	public void shouldFeed() {
-//		// arrange
-//		VirtualPet underTest = new VirtualPet(0, 0, 0);
-//
-//		// act
-//		String response = underTest.feed(1);
-//
-//		// assert
-//		Assert.assertEquals("1", response);
-//	}
+	@Test
+	public void shouldDecreaseHungerBy4() {
+		// arrange
+		VirtualPet underTest = new VirtualPet(5, 5, 5, 5);
+
+		// act
+		underTest.feed(4);
+		int hunger = underTest.getHunger();
+
+		// assert
+		Assert.assertEquals(1 , hunger);
+	}
 }
